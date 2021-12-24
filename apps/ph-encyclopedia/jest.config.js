495f0750c5f65c1ae2bcd409d59b33044217239e
KingDarBoja <stevenbojato04@gmail.com>
2021-12-24 13:37:42 -0500

    initial commit

---
+++ apps/ph-encyclopedia/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'ph-encyclopedia',
+  preset: '../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/ph-encyclopedia',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
