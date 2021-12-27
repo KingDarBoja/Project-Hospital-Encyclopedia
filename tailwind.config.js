62031e7772a209dd2ee6df0fd883c2878e3a5513
KingDarBoja <stevenbojato04@gmail.com>
2021-12-27 12:51:05 -0500

    chore: setup tailwindcss 3

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// const { guessProductionMode } = require("@ngneat/tailwind");
+
+module.exports = {
+  content: [
+    './apps/**/*.{html,ts}',
+    './libs/**/*.{html,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
