6f624d247ee3e8e15d7c3b23dcedf5010175ef71
KingDarBoja <stevenbojato04@gmail.com>
2021-12-27 12:45:09 -0500

    chore: setup angular material

--- apps/ph-encyclopedia/src/app/app.module.ts
+++ apps/ph-encyclopedia/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
