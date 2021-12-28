165f4304db6542ba918faf5eb538bf01450c600c
KingDarBoja <stevenbojato04@gmail.com>
2021-12-28 09:56:39 -0500

    feat: display examination list as card grid

--- apps/ph-encyclopedia/src/app/examination/examination.component.ts
+++ apps/ph-encyclopedia/src/app/examination/examination.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, tap } from 'rxjs/operators';
+import { ProcedureSchema } from '@ph-encyclopedia/shared/procedures';
 
 @Component({
   selector: 'ph-encyclopedia-examination-list',
   templateUrl: 'examination.component.html',
 })
 export class ExaminationComponent {
-  readonly vm$ = this.http.get('assets/procedures/examinations.json').pipe(
-    map((res) => Object.values(res || {})),
-    tap((res) => console.log(res))
-  );
+  readonly vm$ = this.http
+    .get<Record<string, ProcedureSchema>>('assets/procedures/examinations.json')
+    .pipe(
+      map((res) =>
+        Object.values(res || {}).sort((a, b) => a.name.localeCompare(b.name))
+      ),
+      tap((res) => console.log(res))
+    );
 
   constructor(private readonly http: HttpClient) {}
 }
