67afabe9cbbe3c14a817f5b921cac74f15c467af
KingDarBoja <stevenbojato04@gmail.com>
2021-12-29 15:11:48 -0500

    feat: add toolbar with icon and basic routing

--- apps/ph-encyclopedia/src/app/app.module.ts
+++ apps/ph-encyclopedia/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 
@@ -30,6 +33,9 @@ const routes: Routes = [
     BrowserAnimationsModule,
     HttpClientModule,
     RouterModule.forRoot(routes),
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
   ],
   bootstrap: [AppComponent],
 })
