dea8bdd3bfa4e832e0b6ba45fcf35cd622c9cd01
KingDarBoja <stevenbojato04@gmail.com>
2025-07-01 14:20:34 -0500

    chore: migrate nx to latest version 21.2.1

--- apps/ph-encyclopedia/src/app/landing/landing.component.ts
+++ apps/ph-encyclopedia/src/app/landing/landing.component.ts
@@ -1,10 +1,10 @@
-import { CommonModule } from '@angular/common';
+
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 
 @Component({
-    imports: [CommonModule, MatListModule, MatIconModule],
+    imports: [MatListModule, MatIconModule],
     selector: 'ph-encyclopedia-landing-page',
     templateUrl: 'landing.component.html'
 })
